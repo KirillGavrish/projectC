96d07eecc7589e90d73efba4e59bc766b4a04655
Alexandr Shikin <113991711+Foxicc@users.noreply.github.com>
2022-12-19 16:25:28 +0300

    Update kostya.h

--- teamwork/kostya.h
+++ teamwork/kostya.h
@@ -1,1 +1,55 @@
 #include "kirill.h"
+#pragma once
+#include "structs.h"
+#include "functions.h"
+#include "Tree_project2.cpp"
+#include "stb_image.h"
+ 
+color background;
+vec3 light;
+ 
+struct texture
+{
+    color *map;
+    int w, h, n;
+};
+ 
+texture loadtexture(const char *texturefile)
+{
+    int ok, w, h, n;
+    ok = stbi_info(texturefile, &w, &h, &n);
+    unsigned char* image = stbi_load(texturefile &w, &h, &n, STBI_rgb_alpha);
+    return {reinterpret_cast<color *>(image), w, h, n};
+}
+ 
+color getcolor(int w, int h, texture colors)
+{
+    return colors.map[w * colors.w + h];
+}
+ 
+color shade(Ray r, Tree box, color pxcolor)
+{
+    intersection sect = coordinate_of_intersection(box, r, i);
+    if (!happened(sect.i))
+    {
+        return background;
+    }
+ 
+    vec3 AC = sect.tr.a - sect.tr.c;
+    vec3 BC = sect.tr.b - sect.tr.c;
+    vec3 a = AC * sect.tr.a;
+    vec3 b = BC * sect.tr.b;
+    float lc = length(sect.tr.c - sect.position);
+    float lb = length(sect.position - sect.tr.a -  sect.tr.b);
+    float la = length(sect.position - sect.tr.a -  sect.tr.b);
+ 
+    vec3 norm = (sect.tr.n1 * la + sect.face.B.norm * lb + sect.face.C.norm * lc) / (la + lb +lc);
+    ray shadow = {sect.position, normalize(light - sectpos)};
+ 
+    intersection shadowray = coordinate_of_intersection(box, shadow, i);
+ 
+    float brightness = happened(shadowray.i) ? 0 : std::max(0.f, dot(norm, sect.position - light));
+    color lightcolor = {1, 1, 1};
+ 
+    return(pxcolor * (lightcolor * brightness + background * 0,3));
+}
